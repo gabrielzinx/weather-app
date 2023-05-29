4a3471e0f1df9ace51080447bae1dbd92e94d49f
Gabriel <gabriel.soaressc58@gmail.com>
2023-05-29 01:29:45 +0000

    create slider button

--- manage-location/script.js
+++ manage-location/script.js
@@ -1,8 +1,9 @@
 const inputCity = window.document.querySelector("#search-city");
 const listCities = window.document.querySelector("#list-cities");
+const itensCities = listCities.getElementsByTagName('li');
 
-tradeProprietary();
 loadCities();
+tradeProprietary();
 
 inputCity.addEventListener("keydown", (event) => {
     if (event.key === 'Enter') {
@@ -17,7 +18,7 @@ inputCity.addEventListener("keydown", (event) => {
 });
 
 function tradeProprietary() {
-    if (listCities.childElementCount >= 5) {
+    if (listCities.childElementCount >= 4) {
         listCities.style.height = "100%";
     }
 }
@@ -53,4 +54,50 @@ function loadCities() {
         const li = createElementWeatherCity(city);
         listCities.appendChild(li);
     });
+}
+
+function fim(elemento) {
+    console.log('Item ultrapassou 200px para a esquerda:', elemento.textContent);
+}
+
+for (let i = 0; i < itensCities.length; i++) {
+    const item = itensCities[i];
+    let startX = 0;
+    let deltaX = 0;
+
+    item.addEventListener('mousedown', function (event) {
+        startX = event.clientX;
+        item.style.transition = 'all 0s';
+        item.style.cursor = "grabbing";
+        window.document.addEventListener('mousemove', handleMouseMove);
+    });
+
+    item.addEventListener('touchstart', function (event) {
+        startX = event.touches[0].clientX;
+        item.style.transition = 'all 0s';
+    });
+
+    function handleMouseMove(event) {
+        deltaX = event.clientX - startX;
+        item.style.transform = `translateX(${deltaX > 200 ? 200 : deltaX < -200 ? -200 : deltaX}px)`;
+    }
+
+    function resetItem() {
+        item.style.cursor = "grab";
+        item.style.transform = 'none';
+        item.style.transition = 'all 0.8s ease-in-out';
+        item.style.transform = `translateX(${0}px)`;
+        window.document.removeEventListener('mousemove', handleMouseMove);
+    }
+
+    item.addEventListener('touchmove', function (event) {
+        const touch = event.touches[0];
+        deltaX = touch.clientX - startX;
+        item.style.transform = `translateX(${deltaX > 200 ? 200 : deltaX < -200 ? -200 : deltaX}px)`;
+    });
+
+    item.addEventListener('mouseup', resetItem);
+    item.addEventListener('mouseleave', resetItem);
+    item.addEventListener('touchend', resetItem);
+    item.addEventListener('touchcancel', resetItem);
 }
